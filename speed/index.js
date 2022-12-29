296fd04ea4eaa3bf96dc876535aef2db019c3ef1
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2022-12-29 16:53:51 +0300

    Core functionality(speed)

---
+++ speed/index.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const form = document.getElementById('form-elements');
+      form.addEventListener ('submit', (event) => {
+        event.preventDefault ();
+        
+
+        function speedDetector(){
+            const speed = document.getElementById('speed-input').value
+            const points = document.getElementById('result');
+
+            if (speed <0 || speed > 360) {
+                points.innerHTML = "Invalid Speed";
+                return;
+            }else  if (speed <= 70 ){
+                points.innerHTML = "OK"    
+            }else {
+                const demeritPoints = Math.floor((speed - 70) / 5);
+                points.innerHTML = `Points: ${demeritPoints}`;
+                if (demeritPoints > 12){
+                    points.innerHTML = "Licence suspended!"
+                }
+            }
+        }speedDetector()
+    });
+});
